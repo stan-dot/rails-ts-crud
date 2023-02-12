9d80bee0b438f0c00372c803ecc358e4dce0592e
stan-dot <stanislawmalinowski09@gmail.com>
2023-02-12 13:20:23 +0000

    finished the setup

---
+++ v2/drkiq/app/jobs/counter_job.rb
@@ -0,0 +1,6 @@
+class CounterJob < ApplicationJob
+  queue_as :default
+  def perform(*args)
+    21 + 21
+  end
+end
--- v2/drkiq/test/controllers/pages_controller_test.rb
+++ v2/drkiq/test/controllers/pages_controller_test.rb
@@ -1,8 +1,15 @@
 require "test_helper"
 
 class PagesControllerTest < ActionDispatch::IntegrationTest
+  # test "should get home" do
+  #   get pages_home_url
+  #   assert_response :success
+  # end
+
+
+
   test "should get home" do
-    get pages_home_url
+    get "/"
     assert_response :success
   end
 end
