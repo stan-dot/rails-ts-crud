fc18560da0a78391ff5653cffe30c11074d440f0
stan-dot <stanislawmalinowski09@gmail.com>
2023-02-12 13:12:03 +0000

    added a page

--- v2/drkiq/config/routes.rb
+++ v2/drkiq/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root 'pages#home'
+  # get 'pages/home'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
