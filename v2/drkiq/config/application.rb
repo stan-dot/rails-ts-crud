6dad0fab1b7be1e73ba963fe0072d6f8e5bd27d5
stan-dot <stanislawmalinowski09@gmail.com>
2023-02-12 12:40:20 +0000

    needed to chown the file

--- v2/drkiq/config/application.rb
+++ v2/drkiq/config/application.rb
@@ -18,5 +18,13 @@ module Drkiq
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.log_level = :debug
+    config.log_tags  = [:subdomain, :uuid]
+    config.logger    = ActiveSupport::TaggedLogging.new(Logger.new(STDOUT))
+
+    config.cache_store = :redis_store, ENV['CACHE_URL'],
+                         { namespace: 'drkiq::cache' }
+
+    config.active_job.queue_adapter = :sidekiq
   end
 end
